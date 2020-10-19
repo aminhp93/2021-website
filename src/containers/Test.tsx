63c49abbcd53f958cb8901022bd5f14976bcd688
Minh Pham <minhpham@Minhs-MacBook-Pro-2.local>
2020-10-19 15:46:50 +0700

    Add

--- src/containers/Test.tsx
+++ src/containers/Test.tsx
@@ -6,6 +6,34 @@ import { groupBy } from 'lodash';
 import { getListUrlGoValue } from 'reducers/stocks';
 import { getPosts } from 'reducers/post';
 
+import {
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  } from 'recharts';
+  
+
+  const data1 = [
+    {
+      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
+  ];
 interface IProps {
     getListUrlGoValue: any,
     getPosts: any,
@@ -48,10 +76,8 @@ class Test extends React.Component<IProps, IState> {
 
         return Promise.all(listPromises)
             .then(response => {
-                console.log(response)
                 let result = [];
                 response.map(i => result = result.concat(i))
-                console.log(result)
                 result.map(i => i.mappedDate = moment(i.date).format('YYYY-MM-DD'))
                 this.setState({ data: result })
             })
@@ -70,7 +96,6 @@ class Test extends React.Component<IProps, IState> {
 
     render() {
         const { data } = this.state;
-        console.log(groupBy(data, 'mappedDate'))
         const mappedDateObj = groupBy(data, 'mappedDate')
         
         return (
@@ -87,7 +112,7 @@ class Test extends React.Component<IProps, IState> {
                         for (let j=0; j < array.length; j++) {
                             const taggedSymbols = array[j].taggedSymbols;
                             taggedSymbols.map(k => {
-                                const symbol = k.symbol;
+                                const symbol = k.symbol.slice(0,3);
                                 if (Object.keys(objectKeys).indexOf(symbol) === -1) {
                                     objectKeys[symbol] = {
                                         symbol,
@@ -101,20 +126,33 @@ class Test extends React.Component<IProps, IState> {
                                 }
                             })   
                         }
-                        console.log(objectKeys)
+                        
                         const mappedArray = Object.values(objectKeys).sort((a, b) => b.count - a.count).slice(0, 10)
-                        console.log(mappedArray)
+                        
                         return <div>
-                            <div>{i} - {mappedDateObj[i].length}</div>
-                            <div>
-                                {mappedArray.map(item => {
-                                    return <div>{item.symbol} - {item.count}</div>
-                                })}
+                            <div>{i} - {mappedDateObj[i].length}
+                            
+                                <BarChart
+                                    width={500}
+                                    height={100}
+                                    data={mappedArray}
+                                    margin={{
+                                        top: 5, right: 30, left: 20, bottom: 5,
+                                    }}
+                                >
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="symbol" />
+                                    <YAxis />
+                                    <Tooltip />
+                                    <Legend />
+                                    <Bar dataKey="count" fill="#8884d8" />
+                                </BarChart>
                                 <hr/>
                             </div>
                         </div>
                     })}
                 </div>
+                
             </div>
         )
     }
